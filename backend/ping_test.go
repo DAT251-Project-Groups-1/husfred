60216d9896a5e2c90d1d7b1c619255e8654d4c76
Daniel Berge <daniel@berge.us>
2021-02-26 14:17:09 +0100

    Add ping pong

---
+++ backend/ping_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPingPong(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	Ping(c)
+
+	assert.Equal(t, 200, w.Code)
+
+	var got gin.H
+
+	err := json.Unmarshal(w.Body.Bytes(), &got)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := map[string]string{"message": "pong"}
+
+	assert.IsEqual(want, got)
+}
