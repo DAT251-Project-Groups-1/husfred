74e7347f0c7b204beec9b3bd2c4d976d9c86b020
Daniel Berge <daniel@berge.us>
2021-03-19 16:26:07 +0100

    Move setup router

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"cloud.google.com/go/firestore"
-	"firebase.google.com/go/v4/auth"
-	"github.com/DAT251-Project-Groups-1/husfred/controllers"
-	"github.com/DAT251-Project-Groups-1/husfred/middleware"
+	"github.com/DAT251-Project-Groups-1/husfred/config"
 	"github.com/DAT251-Project-Groups-1/husfred/services"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -14,21 +10,6 @@ func main() {
 	auth := services.InitAuth(firebase)
 	firestore := services.InitFirestore(firebase)
 
-	router := SetupRouter(auth, firestore)
+	router := config.SetupRouter(auth, firestore)
 	router.Run()
-}
-
-func SetupRouter(auth *auth.Client, firestore *firestore.Client) *gin.Engine {
-	router := gin.Default()
-	// Enable cors support
-	router.Use(middleware.Cors)
-
-	router.Use(func(ctx *gin.Context) {
-		ctx.Set("firestore", firestore)
-		ctx.Set("auth", auth)
-	})
-
-	household := router.Group("/household")
-	household.POST("/new", controllers.NewHousehold)
-	return router
 }
