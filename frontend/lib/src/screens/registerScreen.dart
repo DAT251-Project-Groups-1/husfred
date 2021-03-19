c680ef127c910cb234357e01450f8177bc612f8a
JanneKH <60662324+JanneKH@users.noreply.github.com>
2021-03-19 17:49:20 +0100

    Join household screen

---
+++ frontend/lib/src/screens/registerScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Register extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
