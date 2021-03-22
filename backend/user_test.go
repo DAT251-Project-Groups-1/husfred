db3761ba599f868056535d004c81e11d14ea6617
Martin Kydland <martin.kydland@student.uib.no>
2021-03-22 14:57:43 +0100

    Added checks for user registration

--- backend/user_test.go
+++ backend/user_test.go
@@ -1,31 +1,68 @@
 package main
+
 import (
 	"bytes"
 	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
 	"github.com/DAT251-Project-Groups-1/husfred/config"
 	"github.com/DAT251-Project-Groups-1/husfred/models"
 	"github.com/DAT251-Project-Groups-1/husfred/services"
 	"github.com/go-playground/assert/v2"
-	"net/http"
-	"net/http/httptest"
-	"testing"
 )
 
-func TestUserRegistration(t *testing.T) {
+func TestUserRegistrationShouldFailIfHouseholdDoesntExist(t *testing.T) {
+
 	firebase := services.InitFirebase()
 	auth := services.InitAuth(firebase)
 	firestore := services.InitFirestore(firebase)
 	router := config.SetupRouter(auth, firestore)
 
 	body := models.User{
-		Name:  "Test User",
+		Name:        "Test User",
+		HouseholdID: "skabbadu",
 	}
 
-	postBody, _:= json.Marshal(body)
+	postBody, _ := json.Marshal(body)
 	requestBody := bytes.NewBuffer(postBody)
 
 	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/user/new", requestBody)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 400, w.Code)
+}
+
+func TestUserRegistrationShouldPassIfHouseholdExists(t *testing.T) {
+	firebase := services.InitFirebase()
+	auth := services.InitAuth(firebase)
+	firestore := services.InitFirestore(firebase)
+	router := config.SetupRouter(auth, firestore)
+
+	w := httptest.NewRecorder()
+
+	//Create a household in order to bind it to the user
+	householdBody := models.Household{
+		Name: "Test House",
+	}
+	postBody, _ := json.Marshal(householdBody)
+	requestBody := bytes.NewBuffer(postBody)
 	req, _ := http.NewRequest("POST", "/household/new", requestBody)
 	router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)}
+	//Create the user using the id of the household
+	userBody := models.User{
+		Name:        "Test user",
+		HouseholdID: w.Body.String(),
+	}
+
+	postBody, _ = json.Marshal(userBody)
+	requestBody = bytes.NewBuffer(postBody)
+
+	req, _ = http.NewRequest("POST", "/user/new", requestBody)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
