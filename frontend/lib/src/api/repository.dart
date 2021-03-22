4c296260c0539e25cd614cead69360a90172a7c7
Marit Lund <maritlund@pc46-196.eduroam.uib.no>
2021-03-22 14:49:09 +0100

    New household frontend

---
+++ frontend/lib/src/api/repository.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:frontend/src/models/household.dart';
+
+const API_URL = String.fromEnvironment('API_URL',
+    defaultValue: 'https://husfred-backend-zprwgvw7pa-ew.a.run.app');
+
+class Repository {
+  Future<void> postPoll(Household household) async {
+    await http.post(Uri.https('$API_URL', '/household/new'),
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(household));
+  }
+}
