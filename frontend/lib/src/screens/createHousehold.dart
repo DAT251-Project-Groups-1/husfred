19fb48d7c1d279952b91f2d306cf26707a0d98ab
Daniel Berge <daniel@berge.us>
2021-03-23 16:40:46 +0100

    Remove pong example

--- frontend/lib/src/screens/createHousehold.dart
+++ frontend/lib/src/screens/createHousehold.dart
@@ -1,28 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:frontend/src/api/api_service.dart';
-import 'package:provider/provider.dart';
 import 'package:frontend/src/models/household.dart';
 import 'package:frontend/src/screens/registerScreen.dart';
-
-// class CreateHousehold extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Create Household"),
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () {
-//             Navigator.pop(context);
-//           },
-//           child: Text('Go back!'),
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:provider/provider.dart';
 
 class CreateHousehold extends StatefulWidget {
   const CreateHousehold({Key? key}) : super(key: key);
