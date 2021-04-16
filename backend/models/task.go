85f69eaf4d0ae4fb983c8d5e924e215b83c81a66
Martin Kydland <martin.kydland@student.uib.no>
2021-04-16 18:15:22 +0200

    bugfix on task complete

--- backend/models/task.go
+++ backend/models/task.go
@@ -3,7 +3,7 @@ package models
 type Task struct {
 	TaskID      string
 	Name        string `binding:"required"`
-	UserID      string `binding:"required"`
+	UserID      string
 	HouseholdID string `binding:"required"`
 	Date        int `binding:"required"`
 	Points		int `binding:"required"`
