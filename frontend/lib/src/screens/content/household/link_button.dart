d2808fd959aa907c873fca9d01cf1d595e29553e
Daniel Berge <daniel@berge.us>
2021-04-19 14:22:07 +0200

    Add uri to clipboard on clikc

--- frontend/lib/src/screens/content/household/link_button.dart
+++ frontend/lib/src/screens/content/household/link_button.dart
@@ -1,4 +1,6 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/src/api/api_service.dart';
 
 class LinkButton extends StatelessWidget {
   @override
@@ -7,7 +9,11 @@ class LinkButton extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: ElevatedButton(
         child: Text('Copy invite link'),
-        onPressed: () async {},
+        onPressed: () {
+          Uri parse = Uri.parse("https://husfred-b5e27.web.app?id=${ApiService.householdID}#/");
+          FlutterClipboard.copy(parse.toString())
+              .then((value) => print(parse.toString()));
+        },
       ),
     );
   }
