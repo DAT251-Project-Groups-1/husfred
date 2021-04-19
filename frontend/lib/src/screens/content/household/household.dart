cea94ee11dca9f611ad42d2ada21c66a3a99d4b4
JanneKH <60662324+JanneKH@users.noreply.github.com>
2021-04-19 11:07:54 +0200

    Added leave button

--- frontend/lib/src/screens/content/household/household.dart
+++ frontend/lib/src/screens/content/household/household.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/src/api/api_service.dart';
 import 'package:frontend/src/screens/content/household/leaderboard_list.dart';
+import 'package:frontend/src/screens/content/household/leavebutton.dart';
 import 'package:provider/provider.dart';
 
 class Household extends StatelessWidget {
@@ -10,11 +11,20 @@ class Household extends StatelessWidget {
       appBar: AppBar(
         title: Text("Household"),
       ),
-      body: FutureBuilder(
-        future: context.read<ApiService>().getLeaderboard(),
-        builder: (context, snapshot) {
-          return LeaderboardList();
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: context.read<ApiService>().getLeaderboard(),
+              builder: (context, snapshot) {
+                return LeaderboardList();
+              },
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+              child: LeaveButton()),
+        ],
       ),
     );
   }
